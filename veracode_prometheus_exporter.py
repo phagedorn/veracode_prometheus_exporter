1051b2eb8d1b4e5f13d65c8e346dd2dfd27963a6
Phillip Hagedorn <phillip.hagedorn@hdi.de>
2024-01-09 19:03:40 +0100

    make custum field configurable

--- veracode_prometheus_exporter.py
+++ veracode_prometheus_exporter.py
@@ -7,30 +7,33 @@ from veracode_api_py import VeracodeAPI
 class ProfileCollector:
     def __init__(self):
         # Enable debug logging for the Veracode API
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.DEBUG)
 
         self.api = VeracodeAPI()
-        self.unique_cinum_count_metric = Gauge('veracode_unique_cinum_count', 'Number of unique CINUMs')
+        self.custom_field_name = os.getenv('CUSTOM_FIELD_NAME', 'cinum')
+        self.unique_custom_field_count_metric = Gauge('veracode_unique_custom_field_count', f'Number of unique {self.custom_field_name}s')
         self.total_app_profiles_metric = Gauge('veracode_total_app_profiles', 'Total number of app profiles')
         self.total_user_count_metric = Gauge('veracode_total_user_count', 'Total number of users')
-        self.count_empty_cinum_custom_fields_metric = Gauge('veracode_total_empty_cinums', 'Total number of empty cinum custom fields')    
-    def get_unique_cinum_count(self):
+        self.count_empty_custom_fields_metric = Gauge('veracode_total_empty_custom_fields', 'Total number of empty custom fields')
+
+    def get_unique_custom_field_count(self):
         apps = self.api.get_apps()
-        cinums = set()
-        not_cinum = 0
+        custom_fields = set()
+        not_custom_field = 0
 
         for app in apps:
-            custom_fields = app['profile']['custom_fields']
-            if custom_fields is not None:
-                for field in custom_fields:
-                    if field['name'] == 'cinum':
-                        cinums.add(field['value'])
+            profile = app['profile']
+            if profile is not None and 'custom_fields' in profile and profile['custom_fields'] is not None:
+                for field in profile['custom_fields']:
+                    if field['name'] == self.custom_field_name:
+                        custom_fields.add(field['value'])
             else:
-                not_cinum += 1
-        self.unique_cinum_count_metric.set(len(cinums))
+                not_custom_field += 1
+
+        self.unique_custom_field_count_metric.set(len(custom_fields))
         self.total_app_profiles_metric.set(len(apps))
-        self.count_empty_cinum_custom_fields_metric.set(not_cinum)
-        return len(cinums)
+        self.count_empty_custom_fields_metric.set(not_custom_field)
+        return len(custom_fields)
 
     def count_users(self):
         users = self.api.get_users()
@@ -45,7 +48,7 @@ if __name__ == '__main__':
 
     start_http_server(port)
     while True:
-        collector.get_unique_cinum_count()
+        collector.get_unique_custom_field_count()
         user_count = collector.count_users()
         collector.total_user_count_metric.set(user_count)
-        time.sleep(60)  
+        time.sleep(60)
